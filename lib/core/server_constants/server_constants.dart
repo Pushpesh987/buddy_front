66c0019f01466270e15343905b89e8a96d1b9697
Pushpesh987 <krpushpesh108@gmail.com>
2024-12-22 09:40:43 +0530

    android release apk done - added proguard-rules.pro

--- lib/core/server_constants/server_constants.dart
+++ lib/core/server_constants/server_constants.dart
@@ -1,7 +1,7 @@
 // import 'dart:io';
 
 class ServerConstant {
-  static String serverURL = 'http://10.0.2.2:3000';
+//   static String serverURL = 'http://10.0.2.2:3000';
 //   static String serverURL = 'https://backend-production-d5d0.up.railway.app';
-//   static String serverURL = 'https://backend-7xif.onrender.com';
+  static String serverURL = 'https://backend-7xif.onrender.com/';
 }
